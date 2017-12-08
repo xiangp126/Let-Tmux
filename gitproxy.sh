8ea91fafdecd2e3ce61105356f98333983c9d899
corsair <xiangp126@126.com>
2017-12-08 11:05:02 -0500

    revised gitproxy.sh

--- gitproxy.sh
+++ gitproxy.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# After this config, git push can be done by username:password,
-# but no ssh-key.
+# config
+# Should be placed at ~/.ssh/config, make it if not exist. 
+# or you should edit line at /etc/ssh/ssh_config
+# 49 #   ProxyCommand ssh -q -W %h:%p gateway.example.com
+# Host *
+#   ProxyCommand netcat -x 127.0.0.1:8080 %h %p
 
 usage() {
     echo "git push using proxy, proxy socks5://127.0.0.0:8080 through SSH reverse tunnel."
-    echo "ssh -vv -ND 8080 midman_server -l log_name"
-    echo "syntax: $0 enable | disable"
+    echo "pls ensure first: ssh -vv -ND 8080 -l [logName] [midmanServer]"
+    echo "syntax: $0 install | uninstall"
 }
 
-proxyAddr="socks5://127.0.0.1:8080"
+# proxycommand use by ssh command.
+pxyCmd='netcat -x 127.0.0.1:8080 %h %p'
 
 if [ $# -le 0 ]; then
     usage
@@ -20,22 +25,50 @@ fi
 # export http_proxy=socks5://127.0.0.1:8080
 # export https_proxy=socks5://127.0.0.1:8080
 
+cfgFile=config
+cfgFilePath=~/.ssh/$cfgFile
+
+uninstall() {
+    if [ -f $cfgFilePath ]; then
+        echo "Found $cfgFile file, Moving $cfgFile to ${cfgFile}.bak"
+        cd ~/.ssh
+        mv $cfgFile ${cfgFile}.bak
+        cd - &>/dev/null
+        echo Move Done!
+    fi
+}
+
+install() {
+    if [ -f ${cfgFilePath} ]; then
+        echo "Found $cfgFile file, backup $cfgFile to ${cfgFile}.bak"
+        cd ~/.ssh
+        mv $cfgFile ${cfgFile}.bak
+        cd - &>/dev/null
+        echo Backup Done!
+    fi
+    # writing comments part to config.
+    echo writing comments part to config ...
+    echo "# config"                                                   >> $cfgFilePath
+    echo "# Should be placed at ~/.ssh/config, make it if not exist." >> $cfgFilePath
+    echo "# or you should edit line at /etc/ssh/ssh_config."          >> $cfgFilePath
+    echo "# 49 #   ProxyCommand ssh -q -W %h:%p gateway.example.com." >> $cfgFilePath
+    
+    # writing main part to config
+    echo writing main part to config ...
+    echo "Host *"                                                   >> $cfgFilePath
+    echo "  ProxyCommand netcat -x 127.0.0.1:8080 %h %p"            >> $cfgFilePath
+    
+    echo "cat ~/.ssh/$cfgFile"
+    cat ~/.ssh/$cfgFile
+}
+
 case $1 in
-    'enable')
-        echo "start enabling http.proxy ..."
-        git config --global http.proxy ${proxyAddr}
-        echo "start enabling https.proxy ..."
-        git config --global https.proxy ${proxyAddr}
+    'install')
+        install
     ;;
 
-    'disable')
-        echo "start disabling http.proxy ..."
-        git config --global --unset http.proxy
-        echo "start disabling https.proxy ..."
-        git config --global --unset https.proxy
+    'uninstall')
+        uninstall
     ;;
 esac;
-
-echo "> git config --global --list"
-git config --global --list
 
